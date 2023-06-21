0a0d270bff8fb4b3d9a392fc42462e8527ec8c62
abdosalama818 <00abdo818@gmail.com>
2023-06-21 17:53:22 +0300

    complete project

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'add-parent' => 'App\\Http\\Livewire\\AddParent',
+  'calendar' => 'App\\Http\\Livewire\\Calendar',
+  'calendar-student' => 'App\\Http\\Livewire\\CalendarStudent',
+  'show-question' => 'App\\Http\\Livewire\\ShowQuestion',
+);
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,133 @@
+<?php return array (
+  'codezero/laravel-unique-translation' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'CodeZero\\UniqueTranslation\\UniqueTranslationServiceProvider',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+  ),
+  'macsidigital/laravel-api-client' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'MacsiDigital\\API\\Providers\\APIServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+  ),
+  'macsidigital/laravel-oauth2-client' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'MacsiDigital\\OAuth2\\Providers\\OAuth2ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+  ),
+  'macsidigital/laravel-zoom' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'MacsiDigital\\Zoom\\Providers\\ZoomServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Zoom' => 'MacsiDigital\\Zoom\\Facades\\Zoom',
+    ),
+  ),
+  'mcamara/laravel-localization' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mcamara\\LaravelLocalization\\LaravelLocalizationServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'LaravelLocalization' => 'Mcamara\\LaravelLocalization\\Facades\\LaravelLocalization',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'spatie/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+  'yoeunes/toastr' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yoeunes\\Toastr\\ToastrServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Toastr' => 'Yoeunes\\Toastr\\Facades\\Toastr',
+    ),
+  ),
+);
